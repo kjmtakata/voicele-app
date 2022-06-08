31f4a1cbeaa2877fd70ee112b0abd334577206ef
Kevin Takata <kjmtakata@gmail.com>
2022-06-08 05:39:15 +0000

    save state to localStorage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   Container,
   Alert,
   Snackbar,
+  CircularProgress,
+  Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import CheckIcon from "@mui/icons-material/Check";
@@ -13,6 +15,7 @@ import ErrorIcon from "@mui/icons-material/Error";
 
 function App() {
   const today = new Date();
+  const dateString = today.toISOString().slice(0, 10);
   const [url, setUrl] = useState(null);
   const [name, setName] = useState(null);
   const [input, setInput] = useState(null);
@@ -20,16 +23,19 @@ function App() {
   const [guesses, setGuesses] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://voicele-api.herokuapp.com/answers/${today
-        .toISOString()
-        .slice(0, 10)}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setUrl(data.url);
-        setName(data.name);
-      });
+    const storedState = JSON.parse(localStorage.getItem(dateString));
+    if (storedState.url && storedState.name && storedState.guesses) {
+      setUrl(() => storedState.url);
+      setName(() => storedState.name);
+      setGuesses(() => storedState.guesses);
+    } else {
+      fetch(`https://voicele-api.herokuapp.com/answers/${dateString}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setUrl(data.url);
+          setName(data.name);
+        });
+    }
 
     fetch("https://voicele-api.herokuapp.com/options")
       .then((response) => response.json())
@@ -38,65 +44,78 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem(
+      dateString,
+      JSON.stringify({ url: url, name: name, guesses: guesses })
+    );
+  }, [url, name, guesses]);
+
   const submitInput = (event) => {
     event.preventDefault();
     setGuesses((guesses) => [...guesses, input]);
   };
 
-  if (url && name) {
-    return (
-      <Container component="main" maxWidth="xs">
-        <Snackbar
-          open={guesses.length >= 6}
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        >
-          <Alert severity="info" sx={{ width: "100%" }}>
-            {name}
-          </Alert>
-        </Snackbar>
+  return (
+    <Container component="main" maxWidth="xs">
+      <Typography variant="h4" align="center" sx={{ mb: 2 }}>
+        Voicele
+      </Typography>
 
-        <audio controls style={{ width: "100%" }}>
-          <source src={url} />
-        </audio>
-
-        {[...Array(6)].map((x, i) => {
-          let severity = "info";
-          let icon = <QuestionMarkIcon />;
-          if (guesses.length > i) {
-            if (guesses[i].toLowerCase() === name.toLowerCase()) {
-              severity = "success";
-              icon = <CheckIcon />;
-            } else {
-              severity = "error";
-              icon = <ErrorIcon />;
-            }
-          }
-          return (
-            <Alert key={i} icon={icon} severity={severity} sx={{ mt: 2 }}>
-              {guesses.length > i ? guesses[i] : ""}
+      {url && name ? (
+        <div>
+          <Snackbar
+            open={guesses.length >= 6}
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          >
+            <Alert severity="info" sx={{ width: "100%" }}>
+              {name}
             </Alert>
-          );
-        })}
+          </Snackbar>
+          <audio controls style={{ width: "100%" }}>
+            <source src={url} />
+          </audio>
 
-        <form onSubmit={submitInput}>
-          <Autocomplete
-            options={options}
-            onChange={(_, value) => setInput(value)}
-            renderInput={(params) => (
-              <TextField {...params} label="Celebrity" />
-            )}
-            sx={{ flexGrow: 1, mt: 3 }}
-          />
+          {[...Array(6)].map((x, i) => {
+            let severity = "info";
+            let icon = <QuestionMarkIcon />;
+            if (guesses.length > i) {
+              if (guesses[i].toLowerCase() === name.toLowerCase()) {
+                severity = "success";
+                icon = <CheckIcon />;
+              } else {
+                severity = "error";
+                icon = <ErrorIcon />;
+              }
+            }
+            return (
+              <Alert key={i} icon={icon} severity={severity} sx={{ mt: 2 }}>
+                {guesses.length > i ? guesses[i] : ""}
+              </Alert>
+            );
+          })}
+          <form onSubmit={submitInput}>
+            <Autocomplete
+              options={options}
+              onChange={(_, value) => setInput(value)}
+              renderInput={(params) => (
+                <TextField {...params} label="Celebrity" />
+              )}
+              sx={{ flexGrow: 1, mt: 3 }}
+            />
 
-          <Button type="submit" sx={{ mt: 2 }} fullWidth variant="contained">
-            Guess
-          </Button>
-        </form>
-      </Container>
-    );
-  } else {
-    return <div>Loading...</div>;
-  }
+            <Button type="submit" sx={{ mt: 2 }} fullWidth variant="contained">
+              Guess
+            </Button>
+          </form>
+        </div>
+      ) : (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress />
+        </div>
+      )}
+    </Container>
+  );
 }
 
 export default App;
